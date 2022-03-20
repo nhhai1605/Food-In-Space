48343af97a5867f181ed9c9466c9233c3f910321
Yuepeng Du (s3698728) <s3698728@student.rmit.edu.au>
2022-03-20 23:11:01 +0800

    spacestation scene upload

---
+++ Assets/ManyWorlds/Space Station/Sci Fi Base/Scripts/DoorActivator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animation))]
+public class DoorActivator : MonoBehaviour 
+{
+	public AnimationClip AnimPlay;
+	public AnimationClip AnimReset;
+	public float TimeToReset;
+	private Animation AnimatorEffect;
+	private float timer;
+	private bool collide = false;
+
+	void Start()
+	{
+		AnimatorEffect = GetComponent<Animation> ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if(timer == 0)
+		{
+		
+			AnimatorEffect.Play(AnimPlay.name);
+			timer = 0f;
+			if (!collide)
+			{
+				collide = true;
+			}
+
+		}
+	}
+
+	void Update()
+	{
+		if (collide) 
+		{
+			timer = timer + Time.deltaTime;
+
+			if (timer > TimeToReset)
+			{
+				AnimatorEffect.Play(AnimReset.name);
+
+				timer=0f;
+				collide=false;
+			}
+
+		}
+	}
+}
---
+++ Assets/ManyWorlds/Space Station/Sci Fi Base/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+	{
+
+	public Vector3 RotationXYZ  = Vector3.zero;
+	public float Speed = 0.5f;
+
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		gameObject.transform.Rotate (RotationXYZ, Speed);
+	}
+}
