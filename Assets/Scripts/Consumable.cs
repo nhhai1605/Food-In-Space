73a0831528ffdc0e63f0dc1da21e25c260af7d6f
Nguyen Hoang Hai (S3822918) <68266079+nhhai1605@users.noreply.github.com>
2022-04-25 20:40:57 +1000

    Fix wording in UI

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -40,7 +40,8 @@ public class Consumable : MonoBehaviour
             if (index == portions.Length)
             {
                 surveyCanvas.gameObject.SetActive(false);
-                surveyCanvas.GetComponentInChildren<Text>().text = name.Substring(0, name.Length - 6);
+                // surveyCanvas.GetComponentInChildren<Text>().text = name.Substring(0, name.Length - 6);
+                Debug.Log("Survey for: " + name.Substring(0, name.Length - 6));
                 surveyCanvas.GetComponent<PageManagement>().nameOfFood = name.Substring(0, name.Length - 6);
                 surveyCanvas.gameObject.SetActive(true);
             }
