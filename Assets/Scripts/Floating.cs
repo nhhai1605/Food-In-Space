80d218d4335b0c08f49da994505dcb9fce541f97
Daniel Atanasovski <s3658474@student.rmit.edu.au>
2022-04-26 17:53:48 +1000

    Adjusted offset range to be smaller

--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Floating : MonoBehaviour
-{ 
+{
     [SerializeField] private Vector3 offset;
     [SerializeField] private float rotation;
-    
-    void Start () 
+
+    void Start()
     {
-        offset =  new Vector3(Random.Range(-20f, 20f), Random.Range(-40f , 40f),Random.Range(-20f, 20f));
+        offset = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         rotation = Random.Range(-4f, 4f) / 50f;
         transform.GetComponent<Rigidbody>().velocity = offset;
     }
-     
-    void Update () 
+
+    void Update()
     {
-        transform.Rotate(rotation,rotation,rotation);
+        transform.Rotate(rotation, rotation, rotation);
     }
 
 }
