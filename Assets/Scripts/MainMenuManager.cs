abe869af1493c739e7a5460925586d8dfcec4c2d
Daniel Atanasovski <dtatanasovski@gmail.com>
2022-05-05 13:24:50 +1000

    Added Spectator Camera and HighDefinitionScene

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -7,10 +7,17 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField]
     private int lowPolySceneBuildID = 1;
+    
+    [SerializeField]
+    private int highDefSceneBuildID = 2;
 
     public void launchLowPolyScene()
     {
         SceneManager.LoadScene(lowPolySceneBuildID);
+    }
+
+    public void launchHighDefScene() {
+        SceneManager.LoadScene(highDefSceneBuildID);
     }
 
 }
