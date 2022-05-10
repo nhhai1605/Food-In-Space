90d1ea3fb68a3c143bfa0cbff29d9e870a0e00c5
Daniel Atanasovski <s3658474@student.rmit.edu.au>
2022-05-11 00:52:03 +1000

    Added Menu on menu button press

--- Assets/Scripts/Consumer.cs
+++ Assets/Scripts/Consumer.cs
@@ -21,6 +21,12 @@ public class Consumer : MonoBehaviour
         {
             consumable.Consume();
         }
+
+        MainMenuFood mainMenuFood = other.collider.GetComponent<MainMenuFood>();
+        if (mainMenuFood != null)
+        {
+            mainMenuFood.GoToScene();
+        }
     }
 
 }
