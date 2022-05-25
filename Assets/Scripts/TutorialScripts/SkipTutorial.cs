e8327e6f4889b6a36dd6d86b02e5d819b2bb104e
Nguyen Hoang Hai (S3822918) <68266079+nhhai1605@users.noreply.github.com>
2022-05-26 03:54:47 +1000

    Update Spawning location

--- Assets/Scripts/TutorialScripts/SkipTutorial.cs
+++ Assets/Scripts/TutorialScripts/SkipTutorial.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class SkipTutorial : MonoBehaviour
 {
     [SerializeField] private GameObject taskActivator;
+    [SerializeField] private GameObject spawnLocation;
 
     public void Skip()
     {
         taskActivator.SetActive(false);
         this.GetComponent<AudioSource>().Play();
-        GameObject.FindGameObjectWithTag("VR Origin").transform.position = Vector3.zero;
+        if (spawnLocation != null)
+        { 
+            GameObject.FindGameObjectWithTag("VR Origin").transform.position = spawnLocation.transform.position; 
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("VR Origin").transform.position = Vector3.zero;
+        }
         GameObject.FindGameObjectWithTag("VR Origin").transform.rotation = Quaternion.Euler(0, 180, 0);
 
 
